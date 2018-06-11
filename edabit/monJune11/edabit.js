25d6e4a0d1149becb32606f58ded3324aa76c395
JakeBrose <mordecaistudios@gmail.com>
2018-06-11 11:16:07 -0500

    exercise 1

---
+++ edabit/monJune11/edabit.js
@@ -0,0 +1,17 @@
+/////////////////
+// Exercise 1 //
+// Create a function that takes a number as its only argument
+// and returns true if it's less than or equal to zero,
+// otherwise return false.
+
+const lessThanOrEqualToZero = (num) => {
+  if (num <= 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+/////////////////
+// Exercise 2 //
+//
