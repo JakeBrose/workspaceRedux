f2d5ab9a349d739fe7010a99e14efdf7ceb07483
JakeBrose <mordecaistudios@gmail.com>
2018-06-11 12:48:52 -0500

    exercise 5, beat sladen

--- edabit/monJune11/edabit.js
+++ edabit/monJune11/edabit.js
@@ -40,3 +40,19 @@ const filterArray = (arr) => {
 const timeForMilkAndCookies = (date) => {
   return date.getMonth() === 11 && date.getDate() === 24;
 };
+
+/////////////////
+// Exercise 5 //
+// Create a function that takes a number as an argument.
+// Add up all the numbers from 1 to the number you passed to the function.
+// For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+const addUp = (num) => {
+  let base = 0;
+  let sum = 0;
+  while (base <= num){
+    sum += base;
+    base++
+  }
+  return sum;
+};
